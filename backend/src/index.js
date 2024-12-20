7b50df4ffcfe77098d90954e51fee5e31ce5a0e2
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2024-12-20 15:16:32 -0500

    cookie and JWT

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 dotenv.config();
 import { connectDB } from './lib/db.js'; //---importo la conexion a la base de datos
 
@@ -8,12 +9,13 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cookieParser()); //---------usando el coookieParser
 app.get('/', (req, res) => {
   res.send('hola');
 });
 app.use('/api/auth', authRouters);
 
 app.listen(PORT, () => {
-  console.log('Server is running on port ', PORT);
+  console.log('Server is running on port ', PORT, `http://localhost:${PORT}/`);
   connectDB(); //------conecto la base de datos
 });
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import { checkAuth, logout, signup, updateProfile, login } from '../controllers/auth.controller.js';
+import { protectRoute } from '../middleware/auth.middleware.js';
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/logout', logout);
-router.put('/update-profile', updateProfile);
+router.put('/update-profile', protectRoute, updateProfile);
 router.get('/check', checkAuth);
 
 export default router;
