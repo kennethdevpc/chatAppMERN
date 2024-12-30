5ccce97c29dbaaccb63e0812e94617880eb540f4
Kenneth E. Puliche C <kennethdevpc@gmail.com>
2024-12-30 13:24:47 -0500

    corrigiendo error de ruta en index.js del front

--- README.md
+++ README.md
@@ -3492,7 +3492,7 @@
       app.use(express.static(path.join(__dirname, '../frontend/dist'))); //---entonces aqui usa los archivos estaticos
 
       app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
       });
     }
 
--- backend/src/index.js
+++ backend/src/index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../frontend/dist'))); //---entonces aqui usa los archivos estaticos
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../frontend', 'dist', 'index.html'));
   });
 }
 
